7aaca9ca7f68f1b066404c97d4bfd1fcdcf5d431
Nahason <nahason.murithi@student.moringaschool.com>
2023-12-11 11:10:57 +0300

    Complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit
 
 def get_all(session):
-    pass
+    getDogs = session.query(Dog)
+    allDogs = getDogs.all()
+    return allDogs
+    
+
 
 def find_by_name(session, name):
-    pass
+    dog = session.query(Dog).filter(Dog.name.like(f'%{name}%')).first()
+    return dog
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter(Dog.id == id).first()
+    return dog
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
